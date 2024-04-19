f34fba52e7425b547e3860bc77beb3c345dda37b
mathutopia <mathutopia@163.com>
2024-04-19 15:14:39 +0800

    test-index.jl

---
+++ index.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.19.41
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d8475670-fe1b-11ee-1b46-173bc6f6d7dc
+md"""
+这里是一个index， 指向看看效果怎么样
+"""
+
+# ╔═╡ Cell order:
+# ╟─d8475670-fe1b-11ee-1b46-173bc6f6d7dc
